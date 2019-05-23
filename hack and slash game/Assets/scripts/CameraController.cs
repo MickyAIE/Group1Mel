064dfaa127a9f4be1494abb0d3f7708417e9eb27
Rick Taylor <s191694@students.aie.edu.au>
2019-05-23 20:53:34 +1000

    added scripts folder and basic camera script

---
+++ hack and slash game/Assets/scripts/CameraController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float m_DampTime = 0.2f;
+
+
+    public Transform m_target;
+
+    private Vector3 m_MoveVelocity;
+    private Vector3 m_DesiredPosition;
+
+    [SerializeField]
+    private float scrollMult = 6f;
+
+    private void Awake()
+    {
+        m_target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    private void Update()
+    {
+        float scroll = -Input.GetAxis("Mouse ScrollWheel");
+        Camera.main.orthographicSize += scroll * scrollMult;
+        Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize, 8, 15);
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        m_DesiredPosition = m_target.position;
+        transform.position = Vector3.SmoothDamp(transform.position, m_DesiredPosition, ref m_MoveVelocity, m_DampTime);
+    }
+
+
+}

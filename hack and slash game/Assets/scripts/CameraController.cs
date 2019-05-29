d3379908d85d2980a9fc0b621e6fe9e7995bf025
Rick Taylor <s191694@students.aie.edu.au>
2019-05-29 20:52:48 +1000

    finished camera script for now

--- hack and slash game/Assets/scripts/CameraController.cs
+++ hack and slash game/Assets/scripts/CameraController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     //camera controll script must be placed on a camera rig(empty object) with the camera as a child
+    //camera return to home kind of fast could be slowed down if time permits
     //camera script done by rick
 
     public float m_DampTime = 0.2f;
@@ -17,18 +18,16 @@ public class CameraController : MonoBehaviour
     //m_DesiredPosition is where the camera starts
     private Vector3 m_DesiredRotation;
     //like desiredposition m_DesiredRotation is what rotation the camera starts at
-    public GameObject camera;
+    public GameObject cameraObject;
     //a reference to the camera
     private bool RotateAroundPlayer = true;
     // if the camera should rotate around the player (set with m_target)
     private bool LookAtPlayer = false;
-    //i honestly don't know what this does
+    //i honestly don't know what this does but it works so to afraid to remove it
     public float RotationSpeed = 5.0f;
     //how fast the right mouse button rotation is
     private Vector3 _Offset = new Vector3(8, 3, 5);
     //how much of an offset there is for the camera from the player when the camera starts
-    private Vector3 _Home = new Vector3(0, -122, 0);
-    //where the camera should return to if the player hasen't rotated recently
     public float endTimer = 30f;
     //how long the timer lasts
     private float timer = 0f;
@@ -50,9 +49,9 @@ public class CameraController : MonoBehaviour
     }
     private void Update()
     {
-        if(Input.GetButton("Fire2"))
+        if(Input.GetButton("Fire2"))//right mouse button
         {
-            if (RotateAroundPlayer)
+            if (RotateAroundPlayer)//this code calculates and applies the rotation while also starting the timer
             {
                 Quaternion camTurnAngle = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * RotationSpeed, Vector3.up);
 
@@ -62,28 +61,27 @@ public class CameraController : MonoBehaviour
             }
         }
 
-        if (_Timer)
+        if (_Timer)//adds to the timer using delta time
         {
             timer += Time.deltaTime;
             timer = Mathf.Clamp(timer, 0f, Mathf.Infinity);
         }
 
-        if (timer >= endTimer)
+        if (timer >= endTimer)//if timer ends. timer is turned off timer is reset to 0 and the camera returns to a default "home" position
         {
             _Timer = false;
             timer = 0f;
-            _Offset = Quaternion.Euler(_Home).eulerAngles;
-            gameObject.transform.localRotation = Quaternion.Euler(_Home);
+            _Offset = new Vector3(8, 3, 5);
             return;
         }
 
-        if (LookAtPlayer || RotateAroundPlayer)
+        if (LookAtPlayer || RotateAroundPlayer)//keeps the camera pointed at player using m_target
             transform.LookAt(m_target);
-        camera.transform.Translate(camera.transform.forward * Input.GetAxis("Mouse ScrollWheel") * scrollMult, Space.World);
+        cameraObject.transform.Translate(cameraObject.transform.forward * Input.GetAxis("Mouse ScrollWheel") * scrollMult, Space.World);
     }
 
 
-    private void Move()
+    private void Move()//causes the movement. when rotating and when player moves
     {
         m_DesiredPosition = m_target.position + _Offset;
         transform.position = Vector3.SmoothDamp(transform.position, m_DesiredPosition, ref m_MoveVelocity, m_DampTime);

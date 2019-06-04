6a1ffe1f567e6a3ec33120a65aaaa2cd2254280b
Rick Taylor <s191694@students.aie.edu.au>
2019-06-04 20:58:36 +1000

    made blocky weapons and started animations to weild them

---
+++ hack and slash game/Assets/scripts/WeaponSelect.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSelect : MonoBehaviour
+{
+    public Animator animation_controller;
+    public GameObject _Sword;
+
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            animation_controller.SetTrigger("1hsword");
+            _Sword.SetActive(true);
+        }
+    }
+}

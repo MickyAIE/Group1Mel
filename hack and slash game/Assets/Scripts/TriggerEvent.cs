5171316c4cb90f8cdc5d55526e382709fc666a42
Jarrod Fox-Kluytmans <s191691@students.aie.edu.au>
2019-06-06 18:17:49 +1000

    Jarrods Work update

---
+++ hack and slash game/Assets/Scripts/TriggerEvent.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerEvent : MonoBehaviour {
+
+    public string _sceneName = string.Empty;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.gameObject.name);
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Application.LoadLevel(_sceneName);
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
